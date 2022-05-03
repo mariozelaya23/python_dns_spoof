548b7dbf7c28d836108f624b2d09358a7252ebbc
Mario Zelaya <mariozelaya23@gmail.com>
2022-05-03 15:50:46 -0500

    adding if statement to show the DNSRR

--- dns_spoof.py
+++ dns_spoof.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 import netfilterqueue
+import scapy.all as scapy
 
 
 def process_packet(packet):
-    print(packet)
+    scapy_packet = scapy.IP(packet.get_payload())
+    if scapy_packet.haslayer(scapy.DNSRR):
+        print(scapy_packet.show())
     # packet.drop()
     packet.accept()
 
